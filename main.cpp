5ef49b88ed6757a6610915539a7a3ef4b7ad848f
Locketgoma <LocketGoma@users.noreply.github.com>
2019-11-20 23:36:34 +0900

    15 > 17
    Signed-off-by: Locketgoma <LocketGoma@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -12,7 +12,7 @@ using namespace std;
 void onMouse(int event, int x, int y, int flags, void* param);
 cv::Mat ReverseColor(cv::Mat image);
 void Lightning(cv::Mat image);
-cv::Mat lineing(cv::Mat image,int start,int end,int max);
+cv::Mat lineing(cv::Mat image, int start, int end, int max);
 cv::Mat Sharpning(cv::Mat image, float, float, float, float, float);
 
 int main()
@@ -36,14 +36,14 @@ int main()
 		std::cout << "[!] 잘못된 경로입니다. 샘플 파일이 실행됩니다." << std::endl;
 		img = cv::imread("sample.jpg", CV_LOAD_IMAGE_COLOR);
 		img_gray = cv::imread("sample.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-		
+
 	}
 
 	//수치 입력 부분
 	int start;
 	int end;
 	int max;
-		
+
 	cout << "샘플링에서 제거할 밝기 영역을 입력해주세요, start < x < end" << endl;
 	cout << "default : 145~165" << endl;
 	cin >> start >> end;
@@ -78,7 +78,7 @@ int main()
 
 	//수치 입력부분 끝
 
-	cv::Mat answer = Sharpning(lineing(img_gray, start, end, max),sigX,sigY,alpha,beta,gamma);
+	cv::Mat answer = Sharpning(lineing(img_gray, start, end, max), sigX, sigY, alpha, beta, gamma);
 
 	//--original
 
@@ -96,7 +96,7 @@ int main()
 
 
 
-	
+
 	//Lightning(img_gray);
 	cv::waitKey(33);
 	//cv::destroyWindow("EXAM");
@@ -113,7 +113,7 @@ int main()
 
 }
 void onMouse(int event, int x, int y, int flags, void* param) {
-	cv::Mat *im = reinterpret_cast<cv::Mat*>(param);
+	cv::Mat* im = reinterpret_cast<cv::Mat*>(param);
 
 	uchar r;
 	uchar g;
@@ -129,7 +129,7 @@ void onMouse(int event, int x, int y, int flags, void* param) {
 		cout << "(" << x << "," << y << ")";
 		//cout << static_cast<int>(im->at<uchar>(cv::Point(x, y)));
 		cout << " // (" << (int)r << "," << (int)g << "," << (int)b << ")" << endl;
-		}
+	}
 	}
 }
 cv::Mat ReverseColor(cv::Mat image) {
@@ -155,8 +155,8 @@ cv::Mat ReverseColor(cv::Mat image) {
 	return reversed;
 }
 void Lightning(cv::Mat image) {
-//밝기값 출력.
-//주변 샘플링
+	//밝기값 출력.
+	//주변 샘플링
 	int sampling = 7;	//n*n;
 	int temp_phase = 0;
 	int x_remain = image.size().width % sampling;
@@ -164,31 +164,31 @@ void Lightning(cv::Mat image) {
 	int add = 0;
 
 
-	for (int i = 0; i < image.size().height- y_remain; i = i + sampling) {
-		for (int j = 0; j < image.size().width- x_remain; j = j + sampling) {
+	for (int i = 0; i < image.size().height - y_remain; i = i + sampling) {
+		for (int j = 0; j < image.size().width - x_remain; j = j + sampling) {
 			//cout << (int)image.at<uchar>(i, j)<<" ";
 			for (int k = 0; k < sampling; k++) {
 				for (int y = 0; y < sampling; y++) {
 					add = (int)image.at<uchar>(i + y, j + k);
-					if (add!=255)
-					temp_phase += add;
+					if (add != 255)
+						temp_phase += add;
 				}
 			}
-			
-			if (temp_phase / (sampling*sampling) > 145 && temp_phase / (sampling*sampling) < 165)
-				printf("%3d",0);
+
+			if (temp_phase / (sampling * sampling) > 145 && temp_phase / (sampling * sampling) < 165)
+				printf("%3d", 0);
 			else
-			printf("%3d", temp_phase / (sampling*sampling));
-			
+				printf("%3d", temp_phase / (sampling * sampling));
+
 
 			temp_phase = 0;
-			
+
 		}
 		cout << endl;
 	}
 }
 
-cv::Mat lineing(cv::Mat image , int start, int end, int max) {
+cv::Mat lineing(cv::Mat image, int start, int end, int max) {
 	//라이닝
 	//주변 샘플 채취
 	int sampling = 1;	//n*n;
@@ -196,7 +196,7 @@ cv::Mat lineing(cv::Mat image , int start, int end, int max) {
 	int x_remain = image.size().width % sampling;
 	int y_remain = image.size().height % sampling;
 	int add = 0;
-	
+
 	if (start > end) {
 		start = -1;
 		end = -1;
@@ -204,33 +204,33 @@ cv::Mat lineing(cv::Mat image , int start, int end, int max) {
 	if (start == -1) start = 145;		//흑백 영역 제거 시작지점
 	if (end == -1) end = 165;			//흑백 영역 제거 종료 지점
 	if (max == -1) max = 235;			//흑백 영역 제거 '잔 선' 제거 범위
-	
+
 
 
 
 	//cv::Mat lined = cvCreateImage(cvSize(image.size().width, image.size().height), IPL_DEPTH_8U, 1);
 	cv::Mat lined = image;
-	
+
 	//cout << "phase 2: " << image.size().width - x_remain << endl;
-	for (int i = 0; i < image.size().height- y_remain; i = i + sampling) {
-		for (int j = 0; j < image.size().width- x_remain; j = j + sampling) {
+	for (int i = 0; i < image.size().height - y_remain; i = i + sampling) {
+		for (int j = 0; j < image.size().width - x_remain; j = j + sampling) {
 			//cout << (int)image.at<uchar>(i, j)<<" ";
 			for (int k = 0; k < sampling; k++) {
 				for (int y = 0; y < sampling; y++) {
 					add = (int)image.at<uchar>(i + y, j + k);
-					if (add!=255)
-					temp_phase += add;
+					if (add != 255)
+						temp_phase += add;
 				}
 			}
-			
-			if (temp_phase / (sampling*sampling) > start && temp_phase / (sampling*sampling) < end || temp_phase / (sampling*sampling) > max)
+
+			if (temp_phase / (sampling * sampling) > start&& temp_phase / (sampling * sampling) < end || temp_phase / (sampling * sampling) > max)
 				temp_phase = 0;
 			else
-				temp_phase = temp_phase / (sampling*sampling);
-			
+				temp_phase = temp_phase / (sampling * sampling);
+
 			//printf("%3d", temp_phase);
-			
-			
+
+
 			//cout << temp_phase << ":" << (uchar)temp_phase;
 			for (int k = 0; k < sampling; k++) {
 				for (int y = 0; y < sampling; y++) {
@@ -241,32 +241,32 @@ cv::Mat lineing(cv::Mat image , int start, int end, int max) {
 				}
 			}
 			//cout << temp_phase << ":" << (uchar)temp_phase;
-			
-			temp_phase = 0;					
-			
+
+			temp_phase = 0;
+
 		}
 		//cout << endl;
 	}
 	return lined;
 }
-cv::Mat Sharpning(cv::Mat image, float sigX, float sigY,float alpha,float beta, float gamma) {
-	
-	
-	
+cv::Mat Sharpning(cv::Mat image, float sigX, float sigY, float alpha, float beta, float gamma) {
+
+
+
 	cv::Mat sharped;
 	sharped = image;
 
 	cv::GaussianBlur(image, sharped, cv::Size(0, 0), sigX, sigY);
 	cv::addWeighted(image, alpha, sharped, beta, gamma, image);
 
-	
-	cv::Mat sharpen_mask = (cv::Mat_<char>(3, 3) << -1, -2, -1,
-													-2, 13, -2,
-													-1, -2, -1);
-	cv::filter2D(image, sharped, image.depth(), sharpen_mask);
-	
 
-	
+	cv::Mat sharpen_mask = (cv::Mat_<char>(3, 3) << -1, -2, -1,
+		-2, 13, -2,
+		-1, -2, -1);
+	cv::filter2D(image, sharped, image.depth(), sharpen_mask);
+
+
+
 
 
 
